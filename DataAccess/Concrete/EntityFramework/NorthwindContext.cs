e9f610d077b37ec4132e0baa406e2135ca8765dc
Ali Bozlak <ali-bozlak@outlook.com>
2022-12-10 15:13:49 +0300

    2022-12-10, 15.13 (Modified NorthwindContext class)

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -16,20 +16,36 @@ namespace DataAccess.Concrete.EntityFramework
             optionsBuilder.UseNpgsql("Host=localhost:5432;Database=Northwind;Username=postgres;Password=postgre84,SQL43");
         }
 
-        public DbSet<Product> products { get; set; }
-        public DbSet<Category> categories { get; set; }
-        public DbSet<Customer> customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Order>().ToTable("orders");
-
             modelBuilder.Entity<Order>().Property(order => order.OrderId).HasColumnName("order_id");
             modelBuilder.Entity<Order>().Property(o => o.CustomerId).HasColumnName("customer_id");
             modelBuilder.Entity<Order>().Property(o => o.EmployeeId).HasColumnName("employee_id");
             modelBuilder.Entity<Order>().Property(o => o.OrderDate).HasColumnName("order_date");
             modelBuilder.Entity<Order>().Property(o => o.ShipCity).HasColumnName("ship_city");
+
+            modelBuilder.Entity<Product>().ToTable("products");
+            modelBuilder.Entity<Product>().Property(p => p.ProductId).HasColumnName("product_id");
+            modelBuilder.Entity<Product>().Property(p => p.ProductName).HasColumnName("product_name");
+            modelBuilder.Entity<Product>().Property(p => p.CategoryId).HasColumnName("category_id");
+            modelBuilder.Entity<Product>().Property(p => p.UnitPrice).HasColumnName("unit_price");
+            modelBuilder.Entity<Product>().Property(p => p.UnitsInStock).HasColumnName("units_in_stock");
+
+            modelBuilder.Entity<Category>().ToTable("categories");
+            modelBuilder.Entity<Category>().Property(c => c.CategoryId).HasColumnName("category_id");
+            modelBuilder.Entity<Category>().Property(c => c.CategoryName).HasColumnName("category_name");
+
+            modelBuilder.Entity<Customer>().ToTable("customers");
+            modelBuilder.Entity<Customer>().Property(c => c.CustomerId).HasColumnName("customer_id");
+            modelBuilder.Entity<Customer>().Property(c => c.CompanyName).HasColumnName("campany_name");
+            modelBuilder.Entity<Customer>().Property(c => c.ContactName).HasColumnName("contact_name");
+            modelBuilder.Entity<Customer>().Property(c => c.City).HasColumnName("city");
         }
     }
 }
